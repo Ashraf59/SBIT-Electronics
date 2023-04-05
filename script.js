3133dc3f0f48fd7f0ca8b27f0fd04deb75302cfb
Ashraf59 <ashrafams59b@gmail.com>
2023-04-05 18:10:33 +0600

    I completed second slider

--- script.js
+++ script.js
@@ -31,4 +31,20 @@ menuItems.forEach(
   }
 )
 
+
+//Hamburger menu hide after scrolling 200 px
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".hamburger").style.top = "30px";
+    document.querySelector(".zindex").style.top = "30px";
+  } else {
+    document.querySelector(".hamburger").style.top = "-200px";
+    document.querySelector(".zindex").style.top  = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
  /* Hamburger Menu End */
